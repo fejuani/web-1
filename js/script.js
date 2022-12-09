218f555a2da5f5a9837b26da410dce63c7c3cf60
fejuani <103078863+fejuani@users.noreply.github.com>
2022-12-08 21:13:01 -0300

    primeiro envio projeto final

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var roubo = document.getElementById("roubo");
+var cadastro = document.getElementById("cadastrar")
+
+function roubar(){
+
+    alert("voce foi robado otarrio kaka");
+}
+function usuario(){
+
+    alert("Usuário cadastrado com sucesso!!");
+}
